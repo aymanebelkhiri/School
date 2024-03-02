43306553e64753b66409a1eeaebd8da444dac980
Aymane <belkhiriaymane5@gmail.com>
2024-03-02 02:35:31 +0100

    modify front end

---
+++ School_management/resources/views/header.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>@yield("titre")</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="{{ url('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="{{ url('css/fontawesome.css') }}">
+<link rel="stylesheet" href="{{ url('css/templatemo-grad-school.css') }}">
+<link rel="stylesheet" href="{{ url('css/owl.css') }}">
+<link rel="stylesheet" href="{{ url('css/lightbox.css') }}">
+
+<!--
+    
+TemplateMo 557 Grad School
+
+https://templatemo.com/tm-557-grad-school
+
+-->
+  </head>
+
+<body>
+
+   
+  <!--header-->
+  <header class="main-header clearfix" role="header">
+    <div class="logo">
+      <a href="#"><em>Grad</em> School</a>
+    </div>
+    <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
+    <nav id="menu" class="main-nav" role="navigation">
+      <ul class="main-menu">
+        <li><a href="{{route('home')}}">Home</a></li>
+        <li><a href="#section4">Courses</a></li>
+        <!-- <li><a href="#section5">Video</a></li> -->
+        <li><a href="#section6">Contact</a></li>
+        <li><a href="https://templatemo.com" class="external">External</a></li>
+        <li class="has-submenu"><a href="#section2">Connect</a>
+          <ul class="sub-menu">
+            <li><a href="#section2">As Student</a></li>
+            <li><a href="#section3">As Teacher</a></li>
+            <li><a href="#section3">As Admin</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+    @yield("content")
+
+    <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p><i class="fa fa-copyright"></i> Copyright 2020 by Grad School  
+          
+           | Design: <a href="https://templatemo.com" rel="sponsored" target="_parent">TemplateMo</a><br>
+           Distributed By: <a href="https://themewagon.com" rel="sponsored" target="_blank">ThemeWagon</a>
+          
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="{{ url('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<script src="{{ url('js/isotope.min.js') }}"></script>
+<script src="{{ url('js/owl-carousel.js') }}"></script>
+<script src="{{ url('js/lightbox.js') }}"></script>
+<script src="{{ url('js/tabs.js') }}"></script>
+<script src="{{ url('js/video.js') }}"></script>
+<script src="{{ url('js/slick-slider.js') }}"></script>
+<script src="{{ url('js/custom.js') }}"></script>
+
+    <script>
+        //according to loftblog tut
+        $('.nav li:first').addClass('active');
+
+        var showSection = function showSection(section, isAnimate) {
+          var
+          direction = section.replace(/#/, ''),
+          reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+          reqSectionPos = reqSection.offset().top - 0;
+
+          if (isAnimate) {
+            $('body, html').animate({
+              scrollTop: reqSectionPos },
+            800);
+          } else {
+            $('body, html').scrollTop(reqSectionPos);
+          }
+
+        };
+
+        var checkSection = function checkSection() {
+          $('.section').each(function () {
+            var
+            $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+            if (topEdge < wScroll && bottomEdge > wScroll) {
+              var
+              currentId = $this.data('section'),
+              reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+              reqLink.closest('li').addClass('active').
+              siblings().removeClass('active');
+            }
+          });
+        };
+
+        $('.main-menu, .scroll-to-section').on('click', 'a', function (e) {
+          if($(e.target).hasClass('external')) {
+            return;
+          }
+          e.preventDefault();
+          $('#menu').removeClass('active');
+          showSection($(this).attr('href'), true);
+        });
+
+        $(window).scroll(function () {
+          checkSection();
+        });
+    </script>
+</body>
+</html>
