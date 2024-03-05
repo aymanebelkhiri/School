43a5cec7844e2d95752ccf44cb4c385686614da2
narjisse <narjisseelkhayyam>
2024-03-05 14:47:57 +0100

    add exams

--- School_management/app/Http/Controllers/ExamController.php
+++ School_management/app/Http/Controllers/ExamController.php
@@ -1,14 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Exam;
-use Illuminate\Support\facades\DB;
+use App\Models\Exam;
 use Illuminate\Http\Request;
 
 class ExamController extends Controller
 {
     public function getExams(){
-      $exams = DB::table('Exam')->get();
+      $exams = Exam::all();
       return view('etudiant.exams',compact('exams'));
     }
 }
--- School_management/resources/views/etudiant/exams.blade.php
+++ School_management/resources/views/etudiant/exams.blade.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exams</title>
-</head>
-@extends('header')
-@section('content')
-<body>
-    @if(isset($exams))
-    @foreach($exams as $exam)
-    
-</body>
-</html>
+@extends('etudiant.header')
+@section('contentStudent')
+<br><br><br><br><br><br><br>
+
+<div class='container'>
+    <center><h1><i>Planning exams</i></h1>
+    <table class='table table-striped '>
+        <thead>
+            <tr>
+                <th scope='col'>Date et Heure</th>
+                <th scope='col'>Titre</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Module</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($exams as $exam)
+            <tr>
+                <td>{{$exam->Date}} {{$exam->heur}}</td>
+                <td>{{$exam->titre}}</td>
+                <td>{{$exam->disc}}</td>
+                <td>{{$exam->module}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    </center>
+</div>
+@endsection
