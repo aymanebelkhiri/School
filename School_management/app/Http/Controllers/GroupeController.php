9942b8510301ec87a8e593ea5f78e49ff59f5ea6
narjisse <narjisseelkhayyam>
2024-03-06 08:54:48 +0100

    adding

---
+++ School_management/app/Http/Controllers/GroupeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\GroupeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\facades\DB;
+use App\Models\Groupe;
+use App\Models\Filiére;
+
+
+
+class GroupeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $Groupes = DB::table('groupes')
+        ->join('filiéres','filiéres.id','=','groupes.Filiére')
+        ->select('groupes.id_groupe','groupes.Nom','groupes.Effectif','filiéres.Nom as Filiére' )->get();
+        return view('admin.groupes.index',compact('Groupes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $Filiéres = Filiére::all(); 
+        return view('admin.groupes.create',compact('Filiéres'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Nom'=>'required',
+            'Effectif'=>'required',
+            'filiére'=>'required'
+        ]);
+
+        $Filiére = DB::table('filiéres')->where('Nom',$request->filiére)->select('id')->get();
+
+        $Groupe = DB::table('groupes')::create([
+            'Nom'=>$request->Nom,
+            'Effectif'=>$request->Effectif,
+            'Filiére'=>$Filiére
+        ]);
+
+        if($Groupe){
+            $MessageSuccess = 'Groupe added successfully';
+            return view('admin.groupes.create',compact('MessageSuccess'));
+        }else{
+            $MessageEchec = 'Failed to add groupe';
+            return view('admin.groupes.create',compact('MessageEchec'));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
